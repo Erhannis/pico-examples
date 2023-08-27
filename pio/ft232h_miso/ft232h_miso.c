ef2784a144af0d7a01af49a22e3c2f1bf5783fb2
erhannis <eyeillus@gmail.com>
2023-08-26 22:45:05 -0400

    I got data off the ESP - but it had errors in it, so now I'm doublechecking the base transmission code again, and getting periodic skips, didn't I FIX that?!?  That's why I switched back to CPU FIFO.  ...Wait, apparently I DIDN'T switch to CPU FIFO, so HOW DOES THIS WORK *AT ALL*, WAT

--- pio/ft232h_miso/ft232h_miso.c
+++ pio/ft232h_miso/ft232h_miso.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <math.h>
 #include <hardware/vreg.h>
 
@@ -40,7 +42,7 @@ See, for instance, https://forum.micropython.org/viewtopic.php?t=12695
 */
 // int delayMul = PICO_XOSC_STARTUP_DELAY_MULTIPLIER;
 
-#define DATA_RDY 27
+#define DATA_RDY 6
 
 // #define SPI_MODE SPI_MODE0
 #define AUTO_INIT 1
@@ -156,14 +158,15 @@ solder oscillators together
 test max speeds of both modes
 */
 
-uint16_t exchange(uint16_t tx) {
+uint16_t __time_critical_func(exchange)(uint16_t tx) {
   cs_select();
   uint16_t rx = 0;
-  spi_write16_read16_blocking(spi, &tx, &rx, 1);
+  spi_write16_read16_blocking(spi_default, &tx, &rx, 1); //LEAK Could maybe be faster to excerpt code from INSIDE this function
   cs_deselect();
   return rx;
 }
 
+// Not time critical, I guess, given printfs...
 uint16_t print_exchange(uint16_t tx) {
   uint16_t rx = exchange(tx);
   printf("tx/rx %04X/%04X\n", tx, rx);
@@ -171,39 +174,137 @@ uint16_t print_exchange(uint16_t tx) {
 }
 
 uint16_t exchangeBuffer[256];
+// Also not time critical
 void print_exchange_buffer(const uint16_t tx[], int offset, int count) {
   for (int i = 0; i < count; i++) {
     cs_select();
-    exchangeBuffer[i] = hspi->transfer16(tx[offset+i]);
+    spi_write16_read16_blocking(spi_default, &tx[offset+i], &exchangeBuffer[i], 1); //LEAK Could maybe be faster to excerpt code from INSIDE this function
     cs_deselect(); // Apparently I have to toggle nss between words
   }
   for (int i = 0; i < count; i++) {
     printf("tx/rx %04X/%04X\n", tx[offset+i], exchangeBuffer[i]);
   }
-  println();
+  printf("\n");
 }
-void exchange_buffer(const uint16_t tx[], int tx_offset, int count, uint16_t rx[], int rx_offset) {
-  hspi->beginTransaction(SPISettings(spiClk, MSBFIRST, SPI_MODE));
+void __time_critical_func(exchange_buffer)(const uint16_t tx[], int tx_offset, int count, uint16_t rx[], int rx_offset) {
   for (int i = 0; i < count; i++) {
     cs_select();
-    rx[rx_offset+i] = hspi->transfer16(tx[tx_offset+i]);
+    spi_write16_read16_blocking(spi_default, &tx[tx_offset+i], &rx[rx_offset+i], 1); //LEAK Could maybe be faster to excerpt code from INSIDE this function
     cs_deselect(); // Apparently I have to toggle nss between words
   }
 }
 
 void wait_ready() {
-  println("delaying for ready....");
+  printf("delaying for ready....\n");
   for (int i = 0; i < 30; i++) {
     uint16_t rx = print_exchange(0x0000);
     if (rx == 0x0000) {
-      println("Ready!");  
-      println();
+      printf("Ready!\n");
+      printf("\n");
       return;
     }
     sleep_ms(100);
   }
-  println("Not ready... :(");  
-  println();
+  printf("Not ready... :(\n");  
+  printf("\n");
+}
+
+void read_wafer_id() {
+  printf("Reading wafer id...\n");
+  print_exchange_buffer(BS_READ_WAFER_ID, 0, sizeof(BS_READ_WAFER_ID) / sizeof(BS_READ_WAFER_ID[0]));
+  printf("\n");
+}
+
+void processCommand(char* command) {
+  // Compare the received command with predefined commands
+  if (!strcmp(command, "h") || !strcmp(command, "help")) {
+    printf("h/help, id, t####, init, dr, c/s/shutter\n"); //PERIODIC Keep up to date
+  } else if (!strcmp(command, "id")) {
+    read_wafer_id();
+  } else if (strlen(command) == 5 && command[0] == 't') {
+    char *endptr;
+    uint16_t c = strtol(&command[1], &endptr, 16);
+    print_exchange(c);
+  } else if (!strcmp(command, "init")) {
+    //DUMMY Probably do this automatically
+    //DUMMY Load sequencer
+    print_exchange_buffer(BS_SETTNGS_1, 0, sizeof(BS_SETTNGS_1) / sizeof(BS_SETTNGS_1[0]));
+    // skip settings 2, at least for my chips //DUMMY will we ever need to deal with wafer < 13?
+    print_exchange_buffer(BS_SET_MODE_GIM, 0, sizeof(BS_SET_MODE_GIM) / sizeof(BS_SET_MODE_GIM[0]));
+    print_exchange_buffer(BS_SET_MODULATION_FREQUENCY, 0, sizeof(BS_SET_MODULATION_FREQUENCY) / sizeof(BS_SET_MODULATION_FREQUENCY[0]));
+    print_exchange_buffer(BS_SET_INTEGRATION_TIME, 0, sizeof(BS_SET_INTEGRATION_TIME) / sizeof(BS_SET_INTEGRATION_TIME[0]));
+  } else if (!strcmp(command, "c") || !strcmp(command, "s") || !strcmp(command, "shutter")) {
+    if (!BATCH_MODE) {
+      print_exchange(0x8200);
+      print_exchange(0x5801); // Set trigger
+    } else {
+      exchange(0x8200);
+      exchange(0x5801); // Set trigger
+    }
+    
+    int16_t frame[8*8];
+    uint16_t row_buf[24+1];
+    uint8_t row2[24];
+
+    // Read frame
+    pio_sm_put_blocking(pio1, 0, 'F');
+    pio_sm_put_blocking(pio1, 0, 'R');
+    pio_sm_put_blocking(pio1, 0, '\n');
+    unsigned long delay = 0;
+    absolute_time_t start = get_absolute_time();
+    for (int i = 3; i >= 0; i--) {
+      // Wait for data ready
+      absolute_time_t ta = get_absolute_time();
+      while (!gpio_get(DATA_RDY));
+      absolute_time_t tb = get_absolute_time();
+      delay += absolute_time_diff_us(ta, tb);
+
+      exchange_buffer(BS_READ_2ROW, 0, sizeof(BS_READ_2ROW) / sizeof(BS_READ_2ROW[0]), row_buf, 0);
+      //DUMMY I'm dropping the least significant 4 bits
+      for (int j = 0; j < 4; j++) {
+        int k = j*3;
+        uint8_t b1 = (uint8_t)(row_buf[k+1] & 0x00FF);
+        uint8_t b2 = (uint8_t)(row_buf[k+2] & 0x00FF);
+        uint8_t b3 = (uint8_t)(row_buf[k+3] & 0x00FF);
+        uint16_t combinedInt1 = ((uint16_t)b1 << 4) | (b2 >> 4);
+        uint16_t combinedInt2 = (b3 << 4) | (b2 & 0x0F);
+        frame[((  i)*8)+(j*2  )] = (int16_t)(combinedInt1 << 4) >> 4;
+        frame[((  i)*8)+(j*2+1)] = (int16_t)(combinedInt2 << 4) >> 4;
+      }
+      for (int j = 0; j < 4; j++) {
+        int k = (j+4)*3;
+        uint8_t b1 = (uint8_t)(row_buf[k+1] & 0x00FF);
+        uint8_t b2 = (uint8_t)(row_buf[k+2] & 0x00FF);
+        uint8_t b3 = (uint8_t)(row_buf[k+3] & 0x00FF);
+        uint16_t combinedInt1 = ((uint16_t)b1 << 4) | (b2 >> 4);
+        uint16_t combinedInt2 = (b3 << 4) | (b2 & 0x0F);
+        frame[((7-i)*8)+(j*2  )] = (int16_t)(combinedInt1 << 4) >> 4;
+        frame[((7-i)*8)+(j*2+1)] = (int16_t)(combinedInt2 << 4) >> 4;
+      }
+    }
+    absolute_time_t stop = get_absolute_time();
+    if (!BATCH_MODE) {
+      printf("micros elapsed: %ld\n", absolute_time_diff_us(start, stop));
+      printf("micros delay: %ld\n", delay);
+      printf("\n");
+      // printFrameScaled(frame);
+      printf("\n");
+    } else {
+      //stdio_uart_out_chars((char*)frame, sizeof(frame));
+      int sf = sizeof(frame);
+      //printf("%d", sf);
+      for (int i = 0; i < sf; i++) {
+          pio_sm_put_blocking(pio1, 0, ((uint8_t*)frame)[i]);
+      }
+      // uart_write_blocking(uart_default, (uint8_t*)frame, sizeof(frame));
+      // Serial.write((char*)frame, sizeof(frame));
+    }
+  } else if (!strcmp(command, "dr")) {
+    int dataRdy = gpio_get(DATA_RDY);
+    printf("dataRdy: %d\n", dataRdy);
+  } else {
+    printf("Invalid command\n"); // Command not recognized
+  }
 }
 
 void initEpc611() {
@@ -214,7 +315,9 @@ void initEpc611() {
     puts("Default SPI pins were not defined");
 #endif
 
+    // If the params are wrong, see spi_set_format
     spi_init(spi_default, spiClk);
+    spi_set_format(spi_default, 16, SPI_CPOL_0, SPI_CPHA_0, SPI_LSB_FIRST);
 
     gpio_set_function(PICO_DEFAULT_SPI_RX_PIN, GPIO_FUNC_SPI);
     gpio_set_function(PICO_DEFAULT_SPI_SCK_PIN, GPIO_FUNC_SPI);
@@ -244,7 +347,7 @@ void initEpc611() {
 }
 
 int main() {
-    stdio_init_all();
+    // stdio_init_all();
 
     uint vco, postdiv1, postdiv2;
     int maxf = 0;
@@ -254,19 +357,31 @@ int main() {
     int nearest = 0;
     for (int f = 0; f < 1000000; f += 1000) {
         if (check_sys_clock_khz(f, &vco, &postdiv1, &postdiv2)) {
-            printf("%d\n", f);
+            // printf("%d\n", f);
             maxf = f;
             if (abs(target-f) < abs(target-nearest)) {
                 nearest = f;
             }
         }
     }
-    printf("max: %d\n", maxf);
-    printf("nearest: %d\n", nearest);
+    // printf("max: %d\n", maxf);
+    // printf("nearest: %d\n", nearest);
+    // printf("nearest: %d\n", nearest);
+    // printf("nearest: %d\n", nearest);
+    // printf("nearest: %d\n", nearest);
 
     //vreg_set_voltage(VREG_VOLTAGE_1_20);
     set_sys_clock_khz(nearest, true);
 
+    stdio_init_all();
+
+    printf("\n\nStartup!\n\n");
+
+    printf("max: %d\n", maxf);
+    printf("nearest: %d\n", nearest);
+
+    initEpc611();
+    
     uint cf_offset = pio_add_program(pio1, &write2ftdi_program);
     printf("Loaded program at %d\n", cf_offset);
     write2ftdi_forever(pio1, 0, cf_offset, data_pins_8, wr_rd_a0_cs_pin);
@@ -275,6 +390,25 @@ int main() {
         (1u << PIO_SM0_SHIFTCTRL_IN_SHIFTDIR_LSB) |
         (1u << PIO_SM0_SHIFTCTRL_OUT_SHIFTDIR_LSB) |
         0;
+
+    // if (AUTO_SHUTTER) {
+    //     // long t = micros();
+    //     // long diff = t - timer;
+    //     // Serial.printf("out: %ld", diff);
+    //     // timer = t;
+
+    //     while (true) {
+    //         processCommand("s");
+    //     }
+
+    //     // t = micros();
+    //     // diff = t - timer;
+    //     // Serial.printf("in: %ld", diff);
+    //     // timer = t;
+    // } else {
+    //     readSerialCommand();
+    // }
+    //delay(10);
 
     uint32_t count = 0;
     while (true) {
